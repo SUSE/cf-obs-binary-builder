e361ae90c09c4c51261adefd67b84c85ff0ca075
André Duffeck <aduffeck@suse.com>
2018-05-16 14:06:41 +0200

    Fix test if OBS_PROJECT is set locally

--- spec/cf_obs_binary_builder/dependency_spec.rb
+++ spec/cf_obs_binary_builder/dependency_spec.rb
@@ -2,15 +2,20 @@ describe CfObsBinaryBuilder::Dependency, '#obs_project' do
   let(:dependency) { CfObsBinaryBuilder::Dependency.new('1.2.3', '12345') }
 
   context "when OBS_PROJECT env variable is not set" do
+    before(:each) do
+      ENV.delete "OBS_PROJECT"
+    end
+
     it "exits with error" do
       expect{ dependency.obs_project }.to raise_error(RuntimeError, "no OBS_PROJECT environment variable set")
     end
   end
 
   context "when OBS_PROJECT env variable is not set" do
-    before do
+    before(:each) do
       ENV["OBS_PROJECT"] = "home:ObsUser"
     end
+
     it "returns the correct value" do
       expect(dependency.obs_project).to eq("home:ObsUser")
     end
