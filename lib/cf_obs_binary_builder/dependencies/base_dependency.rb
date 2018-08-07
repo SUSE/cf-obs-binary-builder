aea21d044f2986d4ab2d99005daee8f26835cd4e
André Duffeck <aduffeck@suse.com>
2018-08-07 14:49:35 +0200

    Add stack name to dependency filenames
    Signed-off-by: Tim Hardeck <thardeck@suse.com>

--- lib/cf_obs_binary_builder/dependencies/base_dependency.rb
+++ lib/cf_obs_binary_builder/dependencies/base_dependency.rb
@@ -72,4 +72,20 @@ class CfObsBinaryBuilder::BaseDependency
       }
     ].to_yaml
   end
+
+  private
+
+  def rpm_macros
+    <<EOF
+# BEGIN shared macros for all dependency packages
+%if 0%{?is_opensuse}
+%define stack opensuse42
+%else
+%define stack sle12
+%endif
+
+%define otherdir %{_topdir}/OTHER
+# END shared macros
+EOF
+  end
 end
