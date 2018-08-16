3249eb9702537682fd690733a5f2a9da643392f7
André Duffeck <aduffeck@suse.com>
2018-08-16 12:57:21 +0200

    Add support for building libffi

---
+++ lib/cf_obs_binary_builder/dependencies/libffi.rb
@@ -0,0 +1,15 @@
+class CfObsBinaryBuilder::Libffi < CfObsBinaryBuilder::BaseDependency
+  def initialize(version)
+    super(
+      "libffi",
+      version,
+      "ftp://sourceware.org/pub/libffi/libffi-#{version}.tar.gz"
+    )
+  end
+
+  def prepare_sources
+    super
+
+    system("curl -L https://raw.githubusercontent.com/p3ck/restraint/master/third-party/libffi-3.1-toolexeclibdir.patch -O libffi-3.1-toolexeclibdir.patch")
+  end
+end
