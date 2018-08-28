5733e49e730d5c2f255e09a7945f1da310af7911
Tim Hardeck <thardeck@suse.de>
2018-08-28 14:05:43 +0200

    Use download_source and verify_checksum for libmemcache

--- lib/cf_obs_binary_builder/dependencies/libmemcache.rb
+++ lib/cf_obs_binary_builder/dependencies/libmemcache.rb
@@ -8,13 +8,10 @@ class CfObsBinaryBuilder::Libmemcache < CfObsBinaryBuilder::BaseDependency
   end
 
   def prepare_sources
-    super
-
     additional_source = "https://www.cyrusimap.org/releases/cyrus-sasl-2.1.26.tar.gz"
-    log "Downloading the sources in the package directory... (#{additional_source})"
-    File.write(File.basename(additional_source), open(additional_source).read)
-    if Digest::SHA256.file(File.basename(additional_source)).hexdigest != "8fbc5136512b59bb793657f36fadda6359cae3b08f01fd16b3d406f1345b7bc3"
-      raise("Checksum mismatch after download of #{additional_source}")
-    end
+    download_source(additional_source)
+    verify_checksum(additional_source, "8fbc5136512b59bb793657f36fadda6359cae3b08f01fd16b3d406f1345b7bc3")
+
+    super
   end
 end
