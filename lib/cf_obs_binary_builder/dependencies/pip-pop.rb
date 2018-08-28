604cff64084169236cbacd11b3883ebf1949d93d
Tim Hardeck <thardeck@suse.de>
2018-08-28 14:06:22 +0200

    Fix pip-pop structure by adding the second source

--- lib/cf_obs_binary_builder/dependencies/pip-pop.rb
+++ lib/cf_obs_binary_builder/dependencies/pip-pop.rb
@@ -1,4 +1,6 @@
 class CfObsBinaryBuilder::Pippop < CfObsBinaryBuilder::PypiDependency
+  attr_reader :source1
+
   def initialize(version)
     super(
       "pip-pop",
@@ -6,5 +8,23 @@ class CfObsBinaryBuilder::Pippop < CfObsBinaryBuilder::PypiDependency
       "MIT",
       "https://github.com/heroku-python/pip-pop"
     )
+  end
+
+  private
+
+  def pypi_source
+    @source1 = parse_pypi_url("docopt", "0.6.2")
+    super
+  end
+
+  def render_spec_template
+    spec_template = File.read(
+      File.expand_path(File.dirname(__FILE__) + "/../templates/pip-pop.spec.erb"))
+    ERB.new(spec_template).result(binding)
+  end
+
+  def prepare_sources
+    download_source(source1)
+    super
   end
 end
