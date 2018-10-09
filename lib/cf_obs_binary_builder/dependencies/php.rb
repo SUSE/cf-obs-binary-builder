0d625f468c546e946d1d03cb32c0c612cfef8220
Tim Hardeck <thardeck@suse.de>
2018-10-09 14:54:20 +0200

    Add php dependency php

---
+++ lib/cf_obs_binary_builder/dependencies/php.rb
@@ -0,0 +1,211 @@
+class CfObsBinaryBuilder::Php < CfObsBinaryBuilder::BaseDependency
+  attr_reader :major_version, :php_extensions
+
+  def initialize(version)
+    @major_version = version[/^(\d).*/,1]
+    @php_extensions = extract_extensions
+
+    super(
+      "php",
+      version,
+      "https://php.net/distributions/php-#{version}.tar.gz"
+    )
+  end
+
+  def render_spec_template
+    spec_template = File.read(
+      File.expand_path(File.dirname(__FILE__) + "/../templates/php.spec.erb"))
+    ERB.new(spec_template).result(binding)
+  end
+
+  def prepare_sources
+    super
+
+    php_extensions.each do |filename, metadata|
+      source = metadata[:url]
+
+      log "Downloading the sources in the package directory... (#{source})"
+      File.write(filename, open(source).read)
+      verify_checksum(filename, metadata[:md5])
+    end
+
+    # Download snmp-mibs-downloader
+    additional_source = "http://archive.ubuntu.com/ubuntu/pool/multiverse/s/snmp-mibs-downloader/snmp-mibs-downloader_1.1.tar.gz"
+    filename = "extensions-"+ File.basename(additional_source)
+    File.write(filename, open(additional_source).read)
+    verify_checksum(filename, "3001ef3fad46959084cc13a923c98d30")
+
+    # Download geolite ruby scripts
+    File.write("download_geoip_db.rb", open("https://raw.githubusercontent.com/cloudfoundry/binary-builder/master/bin/download_geoip_db.rb").read)
+    File.write("geoip_downloader.rb", open("https://raw.githubusercontent.com/cloudfoundry/binary-builder/master/lib/geoip_downloader.rb").read)
+  end
+
+  def extract_extensions
+    php_extensions_yml = {}
+    Dir.mktmpdir("cf-obs-binary-builder-php") do |tmpdir|
+      filename = @major_version == "7" ? "php7-extensions.yml" : "php-extensions.yml"
+      source = "https://raw.githubusercontent.com/cloudfoundry/public-buildpacks-ci-robots/master/binary-builds/#{filename}"
+      File.write(File.join(tmpdir, File.basename(source)), open(source).read)
+      php_extensions_yml = YAML.load_file(File.join(tmpdir, filename))
+    end
+
+    extensions = {}
+    php_extensions_yml.each do |key, elements|
+      elements.each do |metadata|
+        url = extract_url(metadata)
+        if url
+          suffix = url[/(\.[a-zA-Z]{1,4}\.{0,1}[a-zA-Z]{0,3})$/,1]
+          filename = key + "-" + metadata["name"] + "-" + metadata["version"] + suffix
+
+          extensions[filename] = {
+            md5: metadata["md5"],
+            url: url,
+          }
+        end
+      end
+    end
+
+    extensions
+  end
+
+  def extract_url(metadata)
+    if Module.const_defined?("CfObsBinaryBuilder::Php::" + metadata['klass'])
+      klass = Module.const_get("CfObsBinaryBuilder::Php::" + metadata['klass'])
+      extension = klass.new(metadata["name"], metadata["version"], metadata["md5"])
+      extension.url
+    end
+  end
+
+  class BaseRecipe
+    attr_reader :name, :version, :checksum
+
+    def initialize(name, version, checksum)
+      @name = name
+      @version = version
+      @checksum = checksum
+    end
+  end
+
+  class PeclRecipe < BaseRecipe
+    def url
+      "http://pecl.php.net/get/#{name}-#{version}.tgz"
+    end
+  end
+
+  class AmqpPeclRecipe < PeclRecipe
+  end
+
+  class GeoipRecipe < PeclRecipe
+  end
+
+  class HiredisRecipe < BaseRecipe
+    def url
+      "https://github.com/redis/hiredis/archive/v#{version}.tar.gz"
+    end
+  end
+
+  class LibSodiumRecipe < BaseRecipe
+    def url
+      "https://github.com/jedisct1/libsodium/releases/download/#{version}/libsodium-#{version}.tar.gz"
+    end
+  end
+
+  class IonCubeRecipe < BaseRecipe
+    def url
+      "http://downloads3.ioncube.com/loader_downloads/ioncube_loaders_lin_x86-64_#{version}.tar.gz"
+    end
+  end
+
+  class LibmemcachedRecipe < BaseRecipe
+    def url
+      "https://launchpad.net/libmemcached/1.0/#{version}/+download/libmemcached-#{version}.tar.gz"
+    end
+  end
+
+  class UnixOdbcRecipe < BaseRecipe
+    def url
+      "http://www.unixodbc.org/unixODBC-#{version}.tar.gz"
+    end
+  end
+
+  class LibRdKafkaRecipe < BaseRecipe
+    def url
+      "https://github.com/edenhill/librdkafka/archive/v#{version}.tar.gz"
+    end
+  end
+
+  class CassandraCppDriverRecipe < BaseRecipe
+    def url
+      "https://github.com/datastax/cpp-driver/archive/#{version}.tar.gz"
+    end
+  end
+
+  class LuaPeclRecipe < PeclRecipe
+  end
+
+  class LuaRecipe < BaseRecipe
+    def url
+      "http://www.lua.org/ftp/lua-#{version}.tar.gz"
+    end
+  end
+
+  class MemcachedPeclRecipe < PeclRecipe
+  end
+
+  class PhalconRecipe < PeclRecipe
+    def url
+      "https://github.com/phalcon/cphalcon/archive/v#{version}.tar.gz"
+    end
+  end
+
+  class PHPIRedisRecipe < PeclRecipe
+    def url
+      "https://github.com/nrk/phpiredis/archive/v#{version}.tar.gz"
+    end
+  end
+
+  class RedisPeclRecipe < PeclRecipe
+  end
+
+  class PHPProtobufPeclRecipe < PeclRecipe
+    def url
+      "https://github.com/allegro/php-protobuf/archive/v#{version}.tar.gz"
+    end
+  end
+
+  class TidewaysXhprofRecipe < PeclRecipe
+    def url
+      "https://github.com/tideways/php-xhprof-extension/archive/v#{version}.tar.gz"
+    end
+  end
+
+  class RabbitMQRecipe < BaseRecipe
+    def url
+      "https://github.com/alanxz/rabbitmq-c/archive/v#{version}.tar.gz"
+    end
+  end
+
+  class SuhosinPeclRecipe < PeclRecipe
+    def url
+      "https://github.com/sektioneins/suhosin/archive/#{version}.tar.gz"
+    end
+  end
+
+  class TwigPeclRecipe < PeclRecipe
+    def url
+      "https://github.com/twigphp/Twig/archive/v#{version}.tar.gz"
+    end
+  end
+
+  class XcachePeclRecipe < PeclRecipe
+    def url
+      "http://xcache.lighttpd.net/pub/Releases/#{version}/xcache-#{version}.tar.gz"
+    end
+  end
+
+  class XhprofPeclRecipe < PeclRecipe
+    def url
+      "https://github.com/phacility/xhprof/archive/#{version}.tar.gz"
+    end
+  end
+end
---
+++ spec/cf_obs_binary_builder/dependencies/php_spec.rb
@@ -0,0 +1,22 @@
+describe CfObsBinaryBuilder::Php::BaseRecipe do
+  it "provides name, version and checksum" do
+    object = CfObsBinaryBuilder::Php::BaseRecipe.new(
+      "test", "1.0.0", "51d5827651328236ecb7c60517c701c2"
+    )
+    expect(object.name).to eq("test")
+    expect(object.version).to eq("1.0.0")
+    expect(object.checksum).to eq("51d5827651328236ecb7c60517c701c2")
+  end
+end
+
+describe CfObsBinaryBuilder::Php::PeclRecipe do
+  it "provides name, version, checksum and url" do
+    apcu = CfObsBinaryBuilder::Php::PeclRecipe.new(
+      "apcu", "4.0.11", "13c0c0dd676e5a7905d54fa985d0ee62"
+    )
+    expect(apcu.name).to eq("apcu")
+    expect(apcu.version).to eq("4.0.11")
+    expect(apcu.checksum).to eq("13c0c0dd676e5a7905d54fa985d0ee62")
+    expect(apcu.url).to eq("http://pecl.php.net/get/apcu-4.0.11.tgz")
+  end
+end
