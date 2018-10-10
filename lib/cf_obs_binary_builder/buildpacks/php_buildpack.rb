66f8cda3d31d002f9b8f849e0b0c2f1385ddca15
Tim Hardeck <thardeck@suse.de>
2018-10-10 15:28:21 +0200

    Switch php buildpack to ruby buildpack-packager

--- lib/cf_obs_binary_builder/buildpacks/php_buildpack.rb
+++ lib/cf_obs_binary_builder/buildpacks/php_buildpack.rb
@@ -2,4 +2,10 @@ class CfObsBinaryBuilder::PhpBuildpack < CfObsBinaryBuilder::BaseBuildpack
   def initialize(upstream_version, revision)
     super("php", upstream_version, revision)
   end
+
+  def render_spec_template
+    spec_template = File.read(
+      File.expand_path(File.dirname(__FILE__) + "/../templates/buildpack-ruby-based.spec.erb"))
+    ERB.new(spec_template).result(binding)
+  end
 end
