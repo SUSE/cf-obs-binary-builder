b90f831152eac4337c6822bc6a173f7e7650602a
Tim Hardeck <thardeck@suse.de>
2019-01-02 16:38:56 +0100

    Use downstream cyrus-sasl instead of upstream

--- lib/cf_obs_binary_builder/dependencies/libmemcache.rb
+++ lib/cf_obs_binary_builder/dependencies/libmemcache.rb
@@ -6,12 +6,4 @@ class CfObsBinaryBuilder::Libmemcache < CfObsBinaryBuilder::BaseDependency
       "https://launchpad.net/libmemcached/1.0/#{version}/+download/libmemcached-#{version}.tar.gz"
     )
   end
-
-  def prepare_sources
-    additional_source = "https://www.cyrusimap.org/releases/cyrus-sasl-2.1.26.tar.gz"
-    download_source(additional_source)
-    verify_checksum(additional_source, "8fbc5136512b59bb793657f36fadda6359cae3b08f01fd16b3d406f1345b7bc3")
-
-    super
-  end
 end
