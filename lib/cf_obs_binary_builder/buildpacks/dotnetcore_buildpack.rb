0fc18e106461c723798c21c3dbce35730d23d8de
Ettore Di Giacinto <edigiacinto@suse.com>
2019-01-10 16:31:43 +0100

    Use specific template for dotnet buildpack
    Signed-off-by: Christian Richter <crichter@suse.com>

--- lib/cf_obs_binary_builder/buildpacks/dotnetcore_buildpack.rb
+++ lib/cf_obs_binary_builder/buildpacks/dotnetcore_buildpack.rb
@@ -4,6 +4,13 @@ class CfObsBinaryBuilder::DotnetcoreBuildpack < CfObsBinaryBuilder::BaseBuildpac
     super("dotnet-core", upstream_version, revision)
   end
 
+  # Render specific template for dotnet-* which filters the base_dependencies
+  # from BuildRequires
+  def render_spec_template
+    spec_template = File.read(
+      File.expand_path(File.dirname(__FILE__) + "/../templates/buildpack-dotnet-based.spec.erb"))
+    ERB.new(spec_template).result(binding)
+  end
   #
   # def run
   #   @manifest = prepare_sources
