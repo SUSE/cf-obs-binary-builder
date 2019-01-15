9622295ead00fcc1cba0817da42e373cb1b64a38
Christian Richter <crichter@suse.com>
2019-01-15 16:09:52 +0100

    Use buildpack-packager built from obs
    Signed-off-by: Ettore Di Giacinto <edigiacinto@suse.com>

--- lib/cf_obs_binary_builder/buildpacks/base_buildpack.rb
+++ lib/cf_obs_binary_builder/buildpacks/base_buildpack.rb
@@ -64,25 +64,6 @@ class CfObsBinaryBuilder::BaseBuildpack
         raise "Found go "+installed_version[1]+"."+installed_version[2]+" . go >=1.11 is required"
       end
 
-      puts "Buildpack contains go.mod, bundling buildpack-packager"
-
-      if !system("cd #{tarball_dir} && wget https://raw.githubusercontent.com/cloudfoundry/libbuildpack/master/packager/buildpack-packager/main.go -O buildpack-packager.go")
-        raise "Could not download buildpack-packager"
-      end
-
-      Dir.mktmpdir() do |tmpdir|
-        ENV['GOPATH'] = tmpdir
-        if !system("go get github.com/google/subcommands")
-          raise "Could not get github.com/google/subcommands"
-        end
-        if !system("go get github.com/cloudfoundry/libbuildpack")
-          raise "Could not download buildpack-packager"
-        end
-        if !system("pushd #{tmpdir}/src/github.com/cloudfoundry/libbuildpack/packager/buildpack-packager && go build && chmod +x buildpack-packager && popd && mv #{tmpdir}/src/github.com/cloudfoundry/libbuildpack/packager/buildpack-packager/buildpack-packager #{tarball_dir}/buildpack-packager")
-          raise "Could not build buildpack-packager"
-        end
-      end
-
       if !File.directory?(File.join(tarball_dir,'vendor'))
         puts "vendor/ folder absent, generating it"
         if !system("cd #{tarball_dir} && go mod vendor")
