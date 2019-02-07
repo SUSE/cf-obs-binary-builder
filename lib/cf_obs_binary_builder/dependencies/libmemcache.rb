4ee2d93cfed8b30733b980c0ee197a4516ebfb81
Ettore Di Giacinto <edigiacinto@suse.com>
2019-02-07 17:19:55 +0100

    Copy patches for libmemcache also when syncing

--- lib/cf_obs_binary_builder/dependencies/libmemcache.rb
+++ lib/cf_obs_binary_builder/dependencies/libmemcache.rb
@@ -10,8 +10,21 @@ class CfObsBinaryBuilder::Libmemcache < CfObsBinaryBuilder::BaseDependency
     )
   end
 
+  def run(verification_data)
+    basepath = FileUtils.pwd
+    obs_package.create
+    obs_package.checkout do
+      copy_patches()
+      generate_package(verification_data)
+    end
+  end
+
   def prepare_sources
     super
+    copy_patches()
+  end
+
+  def copy_patches()
     log 'Copying patches'
     for patch in patches do
       FileUtils.cp(File.dirname(__FILE__) + "/../templates/patches/#{patch}",".")
