553497411bd1db614981ee07c996472158e9147e
Dimitris Karakasilis <DKarakasilis@suse.com>
2019-02-07 12:31:17 +0200

    Change pip-pop to be a base dependency fetched from GitHub
    because the upstream team is using a fork from a non-canonical location.
    In pypi the version they use doesn't exist.

--- lib/cf_obs_binary_builder/dependencies/pip-pop.rb
+++ lib/cf_obs_binary_builder/dependencies/pip-pop.rb
@@ -1,30 +1,10 @@
-class CfObsBinaryBuilder::Pippop < CfObsBinaryBuilder::PypiDependency
-  attr_reader :source1
+class CfObsBinaryBuilder::Pippop < CfObsBinaryBuilder::BaseDependency
 
   def initialize(version)
     super(
       "pip-pop",
       version,
-      "MIT",
-      "https://github.com/heroku-python/pip-pop"
+      "https://github.com/djoyahoy/pip-pop/archive/v#{version}.tar.gz"
     )
-  end
-
-  private
-
-  def pypi_source
-    @source1 = parse_pypi_url("docopt", "0.6.2")
-    super
-  end
-
-  def render_spec_template
-    spec_template = File.read(
-      File.expand_path(File.dirname(__FILE__) + "/../templates/pip-pop.spec.erb"))
-    ERB.new(spec_template).result(binding)
-  end
-
-  def prepare_sources
-    download_source(source1)
-    super
   end
 end
