927e3f908381d15e6de675c2a88bd34a7f5791df
Ettore Di Giacinto <edigiacinto@suse.com>
2019-02-07 15:24:30 +0100

    Fix building of libmemcached on cfsle15fs stack
    We reuse the same patch for php, as we have the same GCC version in
    SLE15.

--- lib/cf_obs_binary_builder/dependencies/libmemcache.rb
+++ lib/cf_obs_binary_builder/dependencies/libmemcache.rb
@@ -1,9 +1,20 @@
 class CfObsBinaryBuilder::Libmemcache < CfObsBinaryBuilder::BaseDependency
+  attr_reader :patches
   def initialize(version)
+    # NOTE: Those patches are for sle15 only, the template spec applies them only when built against SLE15
+    @patches = ["libmemcached-gcc7-build.patch"]
     super(
       "libmemcache",
       version,
       "https://launchpad.net/libmemcached/1.0/#{version}/+download/libmemcached-#{version}.tar.gz"
     )
+  end
+
+  def prepare_sources
+    super
+    log 'Copying patches'
+    for patch in patches do
+      FileUtils.cp(File.dirname(__FILE__) + "/../templates/patches/#{patch}",".")
+    end
   end
 end
