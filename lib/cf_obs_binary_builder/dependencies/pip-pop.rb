a8223dc724c510bca8c699a5cc5c081c3e2c8aee
Christian Richter <crichter@suse.com>
2019-02-14 10:46:22 +0100

    Fix pip-pop dependency
    Upstream pip-pop dependency is vendoring also docopt.
    We need to create a dist for pip-pop and docopt.
    
    Signed-off-by: Ettore Di Giacinto <edigiacinto@suse.com>

--- lib/cf_obs_binary_builder/dependencies/pip-pop.rb
+++ lib/cf_obs_binary_builder/dependencies/pip-pop.rb
@@ -7,4 +7,9 @@ class CfObsBinaryBuilder::Pippop < CfObsBinaryBuilder::BaseDependency
       "https://github.com/djoyahoy/pip-pop/archive/v#{version}.tar.gz"
     )
   end
+
+  def prepare_sources
+	  super
+	  system("wget https://github.com/docopt/docopt/archive/0.6.2.tar.gz -O 0.6.2.tar.gz")
+  end
 end
