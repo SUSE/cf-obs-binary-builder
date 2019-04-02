eccf2b4063872d195675137fd962eb0ffe9051c2
Ettore Di Giacinto <edigiacinto@suse.com>
2019-04-02 13:57:04 +0200

    Handle blocked state

--- lib/cf_obs_binary_builder/manifest.rb
+++ lib/cf_obs_binary_builder/manifest.rb
@@ -81,6 +81,9 @@ class CfObsBinaryBuilder::Manifest
         when :signing
           puts "signing"
           return :in_process
+        when :blocked
+          puts "blocked"
+          return :in_process
         when :building
           puts "building"
           return :in_process
