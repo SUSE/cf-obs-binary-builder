304c53417e23dc97fda678ecd114662482b54057
Tim Hardeck <thardeck@suse.de>
2019-09-09 14:44:06 +0200

    Remove obsolete libgdiplus patch

--- lib/cf_obs_binary_builder/dependencies/libgdiplus.rb
+++ lib/cf_obs_binary_builder/dependencies/libgdiplus.rb
@@ -7,13 +7,11 @@ class CfObsBinaryBuilder::Libgdiplus < CfObsBinaryBuilder::BaseDependency
         version,
         "https://github.com/mono/libgdiplus/archive/#{version}.tar.gz"
       )
-      @patches = ["libgdiplus_5.6_64bit_comp_issue.patch"]
     end
 
     # Keep patches already in the obs package for libgdiplus
     def regenerate(verification_data)
       obs_package.checkout(reset: true) do
-        copy_patches()
         generate_package(verification_data)
       end
     end
@@ -22,15 +20,7 @@ class CfObsBinaryBuilder::Libgdiplus < CfObsBinaryBuilder::BaseDependency
         basepath = FileUtils.pwd
         obs_package.create
         obs_package.checkout do
-          copy_patches()
           generate_package(verification_data)
         end
-    end
-
-    def copy_patches()
-      log 'Copying patches'
-      for patch in patches do
-        FileUtils.cp(File.dirname(__FILE__) + "/../templates/patches/#{patch}",".")
-      end
     end
 end
